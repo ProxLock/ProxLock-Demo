2a4be63040d97f8b01a24f23c7b7d246dabb2847
Morris Richman <81453549+Mcrich23@users.noreply.github.com>
2025-11-13 00:04:46 -0800

    Initial Commit

---
+++ ProxLock-Demo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ProxLock-Demo
+//
+//  Created by Morris Richman on 11/13/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ProxLock-Demo/ProxLock_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProxLock_DemoApp.swift
+//  ProxLock-Demo
+//
+//  Created by Morris Richman on 11/13/25.
+//
+
+import SwiftUI
+
+@main
+struct ProxLock_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
