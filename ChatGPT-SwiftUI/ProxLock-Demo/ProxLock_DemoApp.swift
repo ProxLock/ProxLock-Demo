d04972ea36b536e6c858432b9a7f0728a70523d9
Morris Richman <81453549+Mcrich23@users.noreply.github.com>
2025-12-03 18:16:50 -0800

    Update ProxLock_DemoApp.swift

--- ChatGPT-SwiftUI/ProxLock-Demo/ProxLock_DemoApp.swift
+++ ChatGPT-SwiftUI/ProxLock-Demo/ProxLock_DemoApp.swift
@@ -9,9 +9,23 @@ import SwiftUI
 
 @main
 struct ProxLock_DemoApp: App {
+    init() {
+        loadRocketSimConnect()
+    }
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
+    }
+    
+    private func loadRocketSimConnect() {
+#if DEBUG
+        guard (Bundle(path: "/Applications/RocketSim.app/Contents/Frameworks/RocketSimConnectLinker.nocache.framework")?.load() == true) else {
+            print("Failed to load linker framework")
+            return
+        }
+        print("RocketSim Connect successfully linked")
+#endif
     }
 }
